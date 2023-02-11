6b9d8d2e93aad911752b66f49b6517512eaa14aa
JoB5nd11 <johannesbendler115@gmail.com>
2023-02-11 18:16:05 +0100

    More Figures

--- simple_PSO.py
+++ simple_PSO.py
@@ -1,9 +1,11 @@
 import numpy as np
 from numpy.random import uniform as U
+import random
 from random import random as rnd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import matplotlib.gridspec as gridspec
 
 # Try a simple iterative approach first
 
@@ -136,20 +138,17 @@ def PSO(grapher: callable, swarm: list, num_iters: int, dt: float, neighbor_sett
         
         for p in swarm:
             p.a -= ((0.9 - 0.4) / num_iters)
-        # print(swarm[0].a)
 
 if __name__ == '__main__':
-    xss = []
-    yss = []
+    xss, yss = [], []
     def grapher(xs, ys):
         xss.append(xs)
         yss.append(ys)
-
+    
     # DEFINE FUNCTION
     A, B = 0, 100
     rosenbrock_func = lambda x: (A - x[0])**2 + B * (x[1] - x[0]**2)**2
     rastrigin_func = lambda x: 2 * 10 + ((x[0]**2 - 10 * np.cos(2 * np.pi * x[0])) + (x[1]**2 - 10 * np.cos(2 * np.pi * x[1])))
-    # both functions seem to work perfectly fine; getting to (0,0) in about ~50 steps
 
     # PARAMETERS -  you can change these!
     x_range = (-3, 3)
@@ -159,38 +158,85 @@ if __name__ == '__main__':
     num_particles = 20
     num_iters = 100
     dt = 0.1
-    neighbor_protocol = "exclusive_global"
-    # choose from: "inclusive_global", "exclusive_global", "inclusive_fixed", "exclusive_fixed"
+    neighbor_protocol = "exclusive_global" # choose from: "inclusive_global", "exclusive_global", "inclusive_fixed", "exclusive_fixed"
     num_neighborhoods = None
-    func = rosenbrock_func
     particle_kwargs = {"remember_global_best_of_all_time": False} #True} # not sure if this is good?
+    
+    # Set Benchmark Function
+    func = rosenbrock_func
 
     # RUN SIMULATION
     swarm = init_swarm(num_particles, func, init_x_range, x_shape, abc, **particle_kwargs)
     PSO(grapher, swarm, num_iters, dt, neighbor_protocol, num_neighborhoods)
 
-    # X, Y needed for the benchmark function
+
+
+    # ANIMATION
+    # Create Benchmark Function
     x = np.arange(*x_range, 0.025)
     y = np.arange(*x_range, 0.025)
     X, Y = np.meshgrid(x, y)
 
-    fig, ax = plt.subplots()
+    # Create Figure, Grid and subplots
+    fig = plt.figure(figsize=(12, 7))
+    gs = gridspec.GridSpec(2, 3)
+    ax_main = fig.add_subplot(gs[0:2, 0:2])
+    ax_global_best = fig.add_subplot(gs[0, 2])
+    ax_all_perf = fig.add_subplot(gs[1, 2])
 
     # Rosenbrock vmax=1000, Rastrigin vmax=60
-    contour = ax.contourf(X, Y, func((X, Y)), 250, vmin=0, vmax=1000, cmap='jet') 
-    ax.set_xlim(x_range)
-    ax.set_ylim(x_range)
-    ax.set_title("Benchmark function: Rosenbrock")
-    ax.set_xlabel("x values")
-    ax.set_ylabel("y values")
-    scat = ax.scatter([x[0] for x in xss[0]], [x[1] for x in xss[0]], c='w', marker="*")
+    if func == rosenbrock_func:
+        contour = ax_main.contourf(X, Y, func((X, Y)), 300, vmin=0, vmax=1000, cmap='jet') 
+        ax_main.set_title("Benchmark function: Rosenbrock")
+    else:
+        contour = ax_main.contourf(X, Y, func((X, Y)), 200, vmin=0, vmax=60, cmap='jet')
+        ax_main.set_title("Benchmark function: Rastrigin")
+        
+    ax_main.set_xlim(x_range)
+    ax_main.set_ylim(x_range)
+    ax_main.set_xlabel("x values")
+    ax_main.set_ylabel("y values")
+    scat = ax_main.scatter([x[0] for x in xss[0]], [x[1] for x in xss[0]], c='w', marker="*")
 
-    def animate(i):
-        scat.set_offsets(xss[i])
-        return scat, 
+    ax_global_best.set_xlim(0, num_iters)
+    ax_global_best.set_ylim(-0.1, min(yss[0]))
+    ax_global_best.set_title("Global Best Performance")
+    ax_global_best.set_xlabel("Iteration")
+    ax_global_best.set_ylabel("Performance Score")
+    line_global_data = []
+    line_global_best,  = ax_global_best.plot([], [], c='#00ee00', lw=2)
 
-    ani = animation.FuncAnimation(fig, animate, frames=len(xss), interval=100, blit=True)
-    # FFwriter = animation.FFMpegWriter(fps=10)
-    # ani.save('rastrigin.mp4', writer=FFwriter)
+    ax_all_perf.set_xlim(0, num_iters)
+    ax_all_perf.set_ylim(-0.1, 120)
+    ax_all_perf.set_title("All Particle Performance")
+    ax_all_perf.set_xlabel("Iteration")
+    ax_all_perf.set_ylabel("Performance Score")
+    line_all_data = [[] for _ in range(num_particles)]
+    # color gen from https://stackoverflow.com/questions/13998901/generating-a-random-hex-color-in-python
+    all_lines = [ax_all_perf.plot([], [], c='#'+'%06x' % random.randint(0, 0xFFFFFF), alpha=0.5)[0] for _ in range(num_particles)]
     
+    plt.tight_layout()
+
+    def animate_main(i):
+        scat.set_offsets(xss[i])
+        return scat,
+    
+    def animate_global_best(i):
+        line_global_data.append(min(yss[i]))
+        line_global_best.set_data([index for index, _ in enumerate(line_global_data)], line_global_data)
+        if max(line_global_data) > ax_global_best.get_ylim()[1]:
+            ax_global_best.set_ylim(-0.1, max(line_global_data))
+        return line_global_best, 
+    
+    def animate_all_perf(i, lines):
+        for j in range(len(lines)):
+            line_all_data[j].append(yss[i][j])
+            lines[j].set_data([index for index, _ in enumerate(line_all_data[j])], line_all_data[j])
+        return lines
+    
+    # ANIMATION SPEED
+    interval = 100
+    ani_main = animation.FuncAnimation(fig, animate_main, frames=len(xss), interval=interval)
+    ani_global_best = animation.FuncAnimation(fig, animate_global_best, frames=len(xss), interval=interval)
+    ani_all_perf = animation.FuncAnimation(fig, animate_all_perf, fargs=([all_lines]), frames=len(xss), interval=interval)
     plt.show()
