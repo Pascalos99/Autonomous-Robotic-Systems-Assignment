15d1ae90cb01ab6b5b5fed702cf3e2b551ece2bf
JoB5nd11 <johannesbendler115@gmail.com>
2023-03-05 11:16:58 +0100

    New Graph

--- Assignment 3a - EA/pascal/simple_GA.py
+++ Assignment 3a - EA/pascal/simple_GA.py
@@ -28,8 +28,11 @@ def basic_n2_GA(fitness, popsize=100, init_x=(-3,3), init_y=(-3,3), avoid_asex=T
     GA.alter_mutation(x={'sigma':sigma_x}, y={'sigma':sigma_y})
     return GA
 
-def animation_visualizer(xss, yss, rosenbrock=True):
+def animation_visualizer(xss, yss, fitness, rosenbrock=True):
     num_particles = len(xss[0])
+
+    best_fitness = [f[0] for f in fitness]
+    avg_fitness = [sum(f)/len(f) for f in fitness]
 
     A, B = 0, 100
     rosenbrock_func = lambda x: (A - x[0])**2 + B * (x[1] - x[0]**2)**2
@@ -50,8 +53,8 @@ def animation_visualizer(xss, yss, rosenbrock=True):
     fig = plt.figure(figsize=(12, 7))
     gs = gridspec.GridSpec(2, 3)
     ax_main = fig.add_subplot(gs[0:2, 0:2])
-    ax_global_best = fig.add_subplot(gs[0, 2])
-    ax_all_perf = fig.add_subplot(gs[1, 2])
+    # ax_global_best = fig.add_subplot(gs[0, 2])
+    ax_all_perf = fig.add_subplot(gs[0:2, 2])
 
     # Rosenbrock vmax=1000, Rastrigin vmax=60
     if rosenbrock:
@@ -67,22 +70,23 @@ def animation_visualizer(xss, yss, rosenbrock=True):
     ax_main.set_ylabel("y values")
     scat = ax_main.scatter([x[0] for x in xss[0]], [x[1] for x in xss[0]], c='w', marker="*")
 
-    ax_global_best.set_xlim(0, len(xss))
-    ax_global_best.set_ylim(-0.1, min(yss[0]))
-    ax_global_best.set_title("Global Best Performance")
-    ax_global_best.set_xlabel("Iteration")
-    ax_global_best.set_ylabel("Performance Score")
-    line_global_data = []
-    line_global_best,  = ax_global_best.plot([], [], c='#00ee00', lw=2)
+    # ax_global_best.set_xlim(0, len(xss))
+    # ax_global_best.set_ylim(-0.1, min(yss[0]))
+    # ax_global_best.set_title("Global Best Performance")
+    # ax_global_best.set_xlabel("Iteration")
+    # ax_global_best.set_ylabel("Performance Score")
+    # line_global_data = []
+    # line_global_best,  = ax_global_best.plot([], [], c='#00ee00', lw=2)
 
-    ax_all_perf.set_xlim(0, len(xss))
-    ax_all_perf.set_ylim(-0.1, 120)
-    ax_all_perf.set_title("All Particle Performance")
+    ax_all_perf.set_xlim(0, len(xss)-1)
+    ax_all_perf.set_ylim(-0.1, 50)
+    ax_all_perf.set_title("Particle Fitness")
     ax_all_perf.set_xlabel("Iteration")
-    ax_all_perf.set_ylabel("Performance Score")
-    line_all_data = [[] for _ in range(num_particles)]
-    # color gen from https://stackoverflow.com/questions/13998901/generating-a-random-hex-color-in-python
-    all_lines = [ax_all_perf.plot([], [], c='#'+'%06x' % random.randint(0, 0xFFFFFF), alpha=0.5)[0] for _ in range(num_particles)]
+    ax_all_perf.set_ylabel("Fitness")
+    line_all_data = [[], []]
+    all_lines = [ax_all_perf.plot([], [], lw=2, alpha=0.8, label="Top Fitness")[0],
+                 ax_all_perf.plot([], [], lw=2, alpha=0.8, label="Average Fitness")[0]]
+    ax_all_perf.legend()
     
     plt.tight_layout()
 
@@ -90,23 +94,24 @@ def animation_visualizer(xss, yss, rosenbrock=True):
         scat.set_offsets(xss[i])
         return scat,
     
-    def animate_global_best(i):
-        line_global_data.append(min(yss[i]))
-        line_global_best.set_data([index for index, _ in enumerate(line_global_data)], line_global_data)
-        if max(line_global_data) > ax_global_best.get_ylim()[1]:
-            ax_global_best.set_ylim(-0.1, max(line_global_data))
-        return line_global_best, 
+    # def animate_global_best(i):
+    #     line_global_data.append(min(yss[i]))
+    #     line_global_best.set_data([index for index, _ in enumerate(line_global_data)], line_global_data)
+    #     if max(line_global_data) > ax_global_best.get_ylim()[1]:
+    #         ax_global_best.set_ylim(-0.1, max(line_global_data))
+    #     return line_global_best, 
     
     def animate_all_perf(i, lines):
-        for j in range(len(lines)):
-            line_all_data[j].append(yss[i][j])
-            lines[j].set_data([index for index, _ in enumerate(line_all_data[j])], line_all_data[j])
+        line_all_data[0].append(best_fitness[i])
+        lines[0].set_data([index for index, _ in enumerate(line_all_data[0])], line_all_data[0])
+        line_all_data[1].append(avg_fitness[i])
+        lines[1].set_data([index for index, _ in enumerate(line_all_data[1])], line_all_data[1])
         return lines
     
     # ANIMATION SPEED
     interval = 100
     ani_main = animation.FuncAnimation(fig, animate_main, frames=len(xss), interval=interval)
-    ani_global_best = animation.FuncAnimation(fig, animate_global_best, frames=len(xss), interval=interval)
+    # ani_global_best = animation.FuncAnimation(fig, animate_global_best, frames=len(xss), interval=interval)
     ani_all_perf = animation.FuncAnimation(fig, animate_all_perf, fargs=([all_lines]), frames=len(xss), interval=interval)
     plt.show()
 
@@ -121,20 +126,20 @@ if __name__ == '__main__':
     fitness, pops = GA_rosenbrock.iterate(50, record_population=True)
     xss = [[[pops[i]['x'][j], pops[i]['y'][j]] for j in range(len(pops[i]['x']))] for i in range(len(pops))]
     yss = [[pops[i]['fitness'][j] for j in range(len(pops[i]['fitness']))] for i in range(len(pops))]
-    plt.plot([sum(f)/len(f) for f in fitness], label='average fitness')
-    plt.plot([f[0] for f in fitness], label='top fitness')
-    plt.legend()
-    plt.title('Rosenbrock')
-    plt.show()
-    animation_visualizer(xss, yss)
+    # plt.plot([sum(f)/len(f) for f in fitness], label='average fitness')
+    # plt.plot([f[0] for f in fitness], label='top fitness')
+    # plt.legend()
+    # plt.title('Rosenbrock')
+    # plt.show()
+    animation_visualizer(xss, yss, fitness)
 
     # Rastrigin
     fitness, pops = GA_rastrigin2.iterate(50, record_population=True)
     xss = [[[pops[i]['x'][j], pops[i]['y'][j]] for j in range(len(pops[i]['x']))] for i in range(len(pops))]
     yss = [[pops[i]['fitness'][j] for j in range(len(pops[i]['fitness']))] for i in range(len(pops))]
-    plt.plot([sum(f)/len(f) for f in fitness], label='average fitness')
-    plt.plot([f[0] for f in fitness], label='top fitness')
-    plt.legend()
-    plt.title('Rastrigin 2D')
-    plt.show()
-    animation_visualizer(xss, yss, False)
+    # plt.plot([sum(f)/len(f) for f in fitness], label='average fitness')
+    # plt.plot([f[0] for f in fitness], label='top fitness')
+    # plt.legend()
+    # plt.title('Rastrigin 2D')
+    # plt.show()
+    animation_visualizer(xss, yss, fitness, False)
