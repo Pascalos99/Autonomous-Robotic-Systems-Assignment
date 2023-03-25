de16e6456319aba7829952b18f7bff0996c4ae27
JoB5nd11 <johannesbendler115@gmail.com>
2023-03-25 19:40:31 +0100

    Update Johannes 25.03

---
+++ Assignment 4 - Localization/code_johannes.py
@@ -0,0 +1,303 @@
+import pygame
+import numpy as np
+from scipy.optimize import least_squares, minimize
+import random
+
+# BG_COLOR = pygame.Color(200,210,210)
+BG_COLOR = '#ffffff'
+WIDTH, HEIGHT = 800, 600
+FPS = 60
+DT = 0.05   # Delta T (time)
+H = 0.001   # ?
+DV = 3      # Delta V (velocity)
+DW = 0.15   # Delta W (angular velocity)
+
+VIEW_DIST = 100 # threshold for seeing landmarks
+history = 2000  # number of history points
+
+class Map:
+    def __init__(self, type='sunflower'):
+        self.num_landmarks = 25
+        self.landmarks = []
+        
+        if type == 'random':
+            self.init_random_landmarks()
+        elif type == 'sunflower':
+            self.init_sunflower_landmarks()
+        
+    def init_random_landmarks(self):
+        for _ in range(self.num_landmarks):
+            self.landmarks.append([
+                random.uniform(0, WIDTH),
+                random.uniform(0, HEIGHT)
+            ])
+        
+    # Thank you: https://stackoverflow.com/questions/9600801/evenly-distributing-n-points-on-a-sphere 
+    # This places all landmarks inside of a circle with same distance to each other
+    def init_sunflower_landmarks(self):
+        indices = np.arange(0, self.num_landmarks, dtype=float) + 0.5
+        rs = np.sqrt(indices/self.num_landmarks)
+        thetas = np.pi * (1 + 5**0.5) * indices
+
+        for r, theta in zip(rs, thetas):
+            self.landmarks.append([
+                r * np.cos(theta) * WIDTH // 2 + WIDTH // 2, 
+                r * np.sin(theta) * HEIGHT // 2 + HEIGHT // 2,
+            ])
+            
+    def get_landmark_positions(self):
+        return self.landmarks
+
+class Simulation:
+    def __init__(self, x, y):
+        self.xy_init = (x, y)
+        self.x = x
+        self.y = y
+        self.theta = 0.
+        self.v = 0.
+        self.w = 0.
+
+        # This matrix shows how certain we are of our current position. 
+        # It will be updated every step
+        self.covariance = np.array([[0.0001, 0.0, 0.0],
+                                    [0.0, 0.0001, 0.0],
+                                    [0.0, 0.0, 0.0001]])
+        
+        self.history_true = [(x, y, 0)]
+        self.history_pred = [(x, y, 0)]
+        self.history_corr = [(x, y, 0)]
+        self.history_covariance = []
+        
+        self.size = 30
+        self.win = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+        self.clock = pygame.time.Clock()
+
+        self.map = Map(type='sunflower')
+        self.landmarks_in_view = []
+
+        self.key_config = {
+            pygame.K_w: lambda: self.setVW(self.v + DV, self.w),
+            pygame.K_s: lambda: self.setVW(self.v - DV, self.w),
+            pygame.K_a: lambda: self.setVW(self.v, self.w - DW),
+            pygame.K_d: lambda: self.setVW(self.v, self.w + DW),
+            pygame.K_x: lambda: self.setVW(0, 0), 
+            pygame.K_r: lambda: self.setXY(*self.xy_init),
+            pygame.K_c: lambda: self.resetHistory()
+        }
+    
+    def setVW(self, vel, ang_vel):
+        self.v, self.w = vel, ang_vel
+    
+    def setXY(self, x, y):
+        self.x, self.y = x, y
+    
+    def resetHistory(self):
+        self.history_true = [(self.x, self.y, self.theta)]
+        self.history_pred = [(self.x, self.y, self.theta)]
+
+    def draw(self):
+        self.win.fill(BG_COLOR)
+        self.draw_landmarks()
+        self.draw_player()
+        self.draw_covariance()
+        
+    def draw_player(self):
+        pygame.draw.circle(self.win, '#0000ff', (self.x, self.y), self.size, 1)
+        pygame.draw.line(self.win, '#ff0000', (self.x, self.y), (
+                self.x + self.size * np.sin(self.theta + 0.5 * np.pi),
+                self.y - self.size * np.cos(self.theta + 0.5 * np.pi),
+            ), 
+            width=2)
+        
+        for i in range(len(self.history_true) - 1)[-history:]:
+            true_pos_1, true_pos_2 = self.history_true[i][:2], self.history_true[i+1][:2]
+            pred_pos_1, pred_pos_2 = self.history_pred[i][:2], self.history_pred[i+1][:2]
+            pygame.draw.line(self.win, '#000000', true_pos_1, true_pos_2, width=2)
+            if i % 10 > 5:
+                pygame.draw.line(self.win, '#000000', pred_pos_1, pred_pos_2, width=2)
+            
+    def draw_landmarks(self):
+        landmarks_pos = self.map.get_landmark_positions()
+        for l_pos in landmarks_pos:
+            pygame.draw.circle(self. win, '#000000', l_pos, 3, 3)
+        for l_pos in self.landmarks_in_view:
+            pygame.draw.line(self.win, '#00bb00', l_pos, (self.x, self.y), width=2)
+
+    def draw_covariance(self):
+        for cov in self.history_covariance:
+            # TODO rotation
+            pygame.draw.ellipse(
+                surface = self.win,
+                color = '#000000',
+                rect = np.abs(cov),
+                width = 1,
+            )
+
+    def next_state(self, state, v, w):
+        x, y, theta = state
+        x = x + v * np.cos(theta) * DT
+        y = y + v * np.sin(theta) * DT
+        theta = theta + w * DT
+        return x, y, theta
+
+    def step(self):
+        state = (self.x, self.y, self.theta)
+        self.x, self.y, self.theta = self.next_state(state, self.v, self.w)
+
+        # Collect landmarks that are in view distance
+        landmarks_pos = self.map.get_landmark_positions()
+        self.landmarks_in_view = []
+        for l_pos in landmarks_pos:
+            if np.sqrt((l_pos[0] - self.x)**2 + (l_pos[1] - self.y)**2) < VIEW_DIST:
+                self.landmarks_in_view.append(l_pos)
+    
+    def pred(self):
+        """PREDICTION STEP 1
+        Formula: µ_t = A_t * µ_{t-1} + B_t * u_t
+        (see Presentation 19 ARS, Slide 15)
+
+            A_t:        Identity matrix (thus can be ignored)
+            µ_{t-1}:    Previous pose
+            B_t:        Update of position
+            u_t:        Variables velocity and angular_velocity
+        """
+        prev_x, prev_y, prev_theta = self.history_pred[-1]
+        new_pose = \
+            np.array([ # µ_{t-1}
+                [prev_x],
+                [prev_y],
+                [prev_theta]
+            ]) + \
+            np.array([ # B_t
+                [DT * np.cos(prev_theta), 0.0],
+                [DT * np.sin(prev_theta), 0.0],
+                [0                      , DT]
+            ]).dot(
+                np.array([self.v, self.w]).reshape((2, 1)) # u_t
+            )
+        
+        new_pose = [new_pose[0, 0], new_pose[1, 0], new_pose[2, 0]]
+
+        # Add simulated noise to new pose:
+        new_pose += np.array([
+            random.normalvariate(0, 0.1),   # Noise on the x value of bot
+            random.normalvariate(0, 0.1),   # Noise on the y value of bot
+            random.normalvariate(0, 0.01)]) # Noise on rotation of bot
+
+
+        """PREDICTION STEP 2
+        Formula: Sigma_t = A_t * Sigma_{t-1} * A_t^T + R_t
+
+            A_t:        Identity matrix (thus can be ignored)
+            Sigma_{t-1}:Covariance matrix at time t (certainty of position)
+            A_t^t:      Transposed identity matrix (can also be ignored)
+            R_t:        Process noise (fixed)
+
+        Effective Formula:
+        Sigma_{t-1} + R_t
+        """
+        R_t = np.array([
+            [random.normalvariate(0, 1), 0.0, 0.0],
+            [0.0, random.normalvariate(0, 1), 0.0],
+            [0.0, 0.0, random.normalvariate(0, 1)],
+        ])
+        self.covariance += R_t
+
+        return new_pose
+
+
+
+    def correct(self):
+        # If enough landmarks are present, the current location can easily be triangulated
+        if len(self.landmarks_in_view) >= 3:
+            return self.triangulate_position()
+        
+        # Here should be some corrections steps
+        # TODO I am too confused to do anything.... Sorry :(
+
+        return self.history_pred[-1]
+
+
+
+    def triangulate_position(self):
+        """
+        For the assignment we can calculate the exact position of the robot from landmarks within sensor range.
+        This can only be done if 3 or more landmarks are in the vision radius of the bot.
+        After the position is triangulated, some noise is added to simulate sensor noise
+        """
+        lms = []    # List of landmarks (just for easier use, not necessary)
+        dists = []  # List of distances for each landmark
+        for i in range(3):
+            lm = self.landmarks_in_view[i]
+            lms.append(lm)
+            dists.append(np.sqrt((lm[0] - self.x)**2 + (lm[1] - self.y)**2))
+        new_position = self.trilaterate(lms[0], lms[1], lms[2], dists[0], dists[1], dists[2])
+
+        # Add simulated sensor noise
+        new_position[0] += random.normalvariate(1, 0.01)
+        new_position[1] += random.normalvariate(1, 0.01)
+
+        # After we got a new position, the bearing of the robot has to be adjusted as well
+        # For that we choose the first landmark
+        new_theta = self.theta 
+    
+        return new_position[0], new_position[1], new_theta # triangulated x and y, and last known theta
+
+    # Created with ChatGPT
+    def trilaterate(self, p1, p2, p3, d1, d2, d3):
+        def objective(x):
+            return np.sum([(np.linalg.norm(x - p1) - d1)**2,
+                        (np.linalg.norm(x - p2) - d2)**2,
+                        (np.linalg.norm(x - p3) - d3)**2])
+        
+        initial_guess = np.array([0, 0])
+        result = minimize(objective, initial_guess)
+        return result.x
+
+
+
+
+    def run(self):
+        is_running = True
+        tick_counter = 0
+        while is_running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    is_running = False
+                    pygame.quit()
+                    quit()
+
+                if event.type == pygame.KEYDOWN:
+                    try:
+                        if event.key in self.key_config.keys():
+                            self.key_config[event.key]()
+                    except KeyError:
+                        pass
+
+                if event.type == pygame.VIDEORESIZE:
+                    self.win = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+
+            self.step()
+            self.history_true.append((self.x, self.y, self.theta))
+            self.history_pred.append(self.pred())
+            self.history_pred[-1] = self.correct()
+
+            # Add new covariance for plotting
+            if tick_counter % (1 * FPS) == 0:
+                self.history_covariance.append((
+                    self.history_pred[-1][0] - self.covariance[0, 0] // 2,
+                    self.history_pred[-1][1] - self.covariance[1, 1] // 2,
+                    self.covariance[0, 0],
+                    self.covariance[1, 1]
+                ))
+            self.draw()
+
+            
+            pygame.display.flip()
+            self.clock.tick(FPS)
+            tick_counter += 1
+
+if __name__ == '__main__':
+    disp = Simulation(400, 300)
+    disp.run()
+    pygame.quit()
--- Assignment 4 - Localization/simple_test.py
+++ Assignment 4 - Localization/simple_test.py
@@ -1,21 +1,20 @@
 import pygame
+from math import sin, cos, pi
 import numpy as np
 import random
 
 BG_COLOR = pygame.Color(200,210,210)
 WIDTH, HEIGHT = 800, 600
 FPS = 60
-DT = 0.05   # Delta T (time)
-H = 0.001   # ?
-DV = 3      # Delta V (velocity)
-DW = 0.15   # Delta W (angular velocity)
-
-VIEW_DIST = 100 # threshold for seeing landmarks
-history = 2000  # number of history points
+DT = 0.05
+H = 0.001
+DV = 3
+DW = 0.15
+history = 2000
 
 class Map:
     def __init__(self, type='sunflower'):
-        self.num_landmarks = 25
+        self.num_landmarks = 10
         self.landmarks = []
         
         if type == 'random':
@@ -30,8 +29,6 @@ class Map:
                 random.uniform(0, HEIGHT)
             ])
         
-    # Thank you: https://stackoverflow.com/questions/9600801/evenly-distributing-n-points-on-a-sphere 
-    # This places all landmarks inside of a circle with same distance to each other
     def init_sunflower_landmarks(self):
         indices = np.arange(0, self.num_landmarks, dtype=float) + 0.5
         rs = np.sqrt(indices/self.num_landmarks)
@@ -42,47 +39,47 @@ class Map:
                 r * np.cos(theta) * WIDTH // 2 + WIDTH // 2, 
                 r * np.sin(theta) * HEIGHT // 2 + HEIGHT // 2,
             ])
+        print(self.landmarks)
             
     def get_landmark_positions(self):
         return self.landmarks
 
-class Simulation:
-    def __init__(self, x, y):
+class DotDisplay:
+    def __init__(self, x, y, size):
         self.xy_init = (x, y)
         self.x = x
         self.y = y
         self.theta = 0.
-        self.vel = 0.
-        self.ang_vel = 0.
-        self.history_true = [(x, y, 0)]
-        self.history_pred = [(x, y, 0)]
-        
-        self.size = 30
+        self.v = 0.
+        self.w = 0.
+        self.Htrue = [(x, y, 0)]
+        self.Hpred = [(x, y, 0)]
+        self.size = size
+        self.color = (0, 0, 255)
+        self.thickness = 1
         self.win = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
         self.clock = pygame.time.Clock()
-
+        
         self.map = Map(type='sunflower')
-        self.landmarks_in_view = []
 
         self.key_config = {
-            pygame.K_w: lambda: self.setVW(self.vel + DV, self.ang_vel),
-            pygame.K_s: lambda: self.setVW(self.vel - DV, self.ang_vel),
-            pygame.K_a: lambda: self.setVW(self.vel, self.ang_vel - DW),
-            pygame.K_d: lambda: self.setVW(self.vel, self.ang_vel + DW),
+            pygame.K_w: lambda: self.setVW(self.v + DV, self.w),
+            pygame.K_s: lambda: self.setVW(self.v - DV, self.w),
+            pygame.K_a: lambda: self.setVW(self.v, self.w - DW),
+            pygame.K_d: lambda: self.setVW(self.v, self.w + DW),
             pygame.K_x: lambda: self.setVW(0, 0), 
             pygame.K_r: lambda: self.setXY(*self.xy_init),
             pygame.K_c: lambda: self.resetHistory()
         }
     
-    def setVW(self, vel, ang_vel):
-        self.vel, self.ang_vel = vel, ang_vel
+    def setVW(self, v2, w2):
+        self.v, self.w = v2, w2
     
-    def setXY(self, x, y):
-        self.x, self.y = x, y
+    def setXY(self, x2, y2):
+        self.x, self.y = x2, y2
     
     def resetHistory(self):
-        self.history_true = [(self.x, self.y, self.theta)]
-        self.history_pred = [(self.x, self.y, self.theta)]
+        self.Htrue, self.Hpred = [(self.x, self.y, self.theta)], [(self.x, self.y, self.theta)]
 
     def draw(self):
         self.win.fill(BG_COLOR)
@@ -91,52 +88,41 @@ class Simulation:
         pygame.display.flip()
         
     def draw_player(self):
-        pygame.draw.circle(self.win, '#0000ff', (self.x, self.y), self.size, 1)
+        pygame.draw.circle(self.win, self.color,
+                           (self.x, self.y), self.size, self.thickness)
         pygame.draw.line(self.win, '#ff0000', (self.x, self.y), (
-                self.x + self.size * np.sin(self.theta + 0.5 * np.pi),
-                self.y - self.size * np.cos(self.theta + 0.5 * np.pi),
-            ), 
-            width=2)
-        
-        for i in range(len(self.history_true) - 1)[-history:]:
-            true_pos_1, true_pos_2 = self.history_true[i][:2], self.history_true[i+1][:2]
-            pred_pos_1, pred_pos_2 = self.history_pred[i][:2], self.history_pred[i+1][:2]
-            pygame.draw.line(self.win, '#00aa00', true_pos_1, true_pos_2)
-            pygame.draw.line(self.win, '#000000', pred_pos_1, pred_pos_2)
+            self.x + self.size * sin(self.theta + 0.5 * pi),
+            self.y - self.size * cos(self.theta + 0.5 * pi),), width=2)
+        for i in range(len(self.Htrue)-1)[-history:]:
+            h1, h2 = self.Htrue[i][:2], self.Htrue[i+1][:2]
+            p1, p2 = self.Hpred[i][:2], self.Hpred[i+1][:2]
+            pygame.draw.line(self.win, '#00aa00', p1, p2)
+            pygame.draw.line(self.win, '#000000', h1, h2)
             
     def draw_landmarks(self):
         landmarks_pos = self.map.get_landmark_positions()
         for l_pos in landmarks_pos:
-            pygame.draw.circle(self. win, '#000000', l_pos, 3, 3)
-        for l_pos in self.landmarks_in_view:
-            pygame.draw.line(self.win, '#000000', l_pos, (self.x, self.y))
+            pygame.draw.circle(self. win, '#1234aa', l_pos, 3, 3)
 
-    def next_state(self, state, vel, ang_vel):
+    def next_state(self, state, v, w):
         x, y, theta = state
-        if abs(ang_vel) > H:
-            x = x + vel * (np.sin(theta + ang_vel * DT) - np.sin(theta)) / ang_vel
-            y = y - vel * (np.cos(theta + ang_vel * DT) - np.cos(theta)) / ang_vel
+        if abs(w) > H:
+            x = x + v * (sin(theta + w * DT) - sin(theta)) / w
+            y = y - v * (cos(theta + w * DT) - cos(theta)) / w
         else:
-            x = x + vel * np.cos(theta) * DT
-            y = y + vel * np.sin(theta) * DT
-        theta = theta + ang_vel * DT
+            x = x + v * cos(theta) * DT
+            y = y + v * sin(theta) * DT
+        theta = theta + w * DT
         return x, y, theta
 
     def step(self):
         state = (self.x, self.y, self.theta)
-        self.x, self.y, self.theta = self.next_state(state, self.vel, self.ang_vel)
-
-        # Collect landmarks in view distance
-        landmarks_pos = self.map.get_landmark_positions()
-        self.landmarks_in_view = []
-        for l_pos in landmarks_pos:
-            if np.sqrt((l_pos[0] - self.x)**2 + (l_pos[1] - self.y)**2) < VIEW_DIST:
-                self.landmarks_in_view.append(l_pos)
+        self.x, self.y, self.theta = self.next_state(state, self.v, self.w)
     
     def pred(self):
         # this is to 'simulate' what it might look like if we have an actual Karman filter with noise implemented
-        return self.next_state(self.history_pred[-1], self.vel * random.normalvariate(1, 0.5), self.ang_vel * random.normalvariate(1, 0.5))
-
+        return self.next_state(self.Hpred[-1], self.v * random.normalvariate(1, 0.5), self.w * random.normalvariate(1, 0.5))
+    
     def run(self):
         is_running = True
         while is_running:
@@ -157,12 +143,12 @@ class Simulation:
                     self.win = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
 
             self.step()
-            self.history_true.append((self.x, self.y, self.theta))
-            self.history_pred.append(self.pred())
+            self.Htrue.append((self.x, self.y, self.theta))
+            self.Hpred.append(self.pred())
             self.draw()
             self.clock.tick(FPS)
 
 if __name__ == '__main__':
-    disp = Simulation(400, 300)
+    disp = DotDisplay(400, 300, 30)
     disp.run()
     pygame.quit()
