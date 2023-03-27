7a9728aec0f454ba778a7b04a2503120a556334d
Cédric Cortenraede <c.cortenraede@outlook.com>
2023-03-27 15:58:51 +0200

    Added predicted orientation

--- Assignment 4 - Localization/code_johannes.py
+++ Assignment 4 - Localization/code_johannes.py
@@ -227,13 +227,14 @@ class Simulation:
             predicted_x = self.history_pred[-1][0]
             predicted_y = self.history_pred[-1][1]
             
-        # TODO: Predict orientation (theta) of robot using first landmark.
-        predicted_theta = self.history_pred[-1][2]
+        # Predict orientation (theta) of robot using first landmark.
+        relative_position = landmarks[0] - [self.x, self.y]
+        predicted_theta = np.arctan2(relative_position[1], relative_position[0]) - bearings[0]
             
         # TODO: Add noise
         z_t = (
             np.array([predicted_x, predicted_y, predicted_theta])
-            #    + np.array([random.normalvariate(0, 1), random.normalvariate(0, 1), random.normalvariate(0, 1)])
+            + np.array([random.normalvariate(0, 0.05), random.normalvariate(0, 0.05), random.normalvariate(0, 0.05)])
         ).T
         
         print("Predicted current location using landmarks:\n", z_t)
@@ -252,9 +253,9 @@ class Simulation:
         Sigma_t = self.covariance
         C_t = np.identity(3)
         Q_t = np.array([
-            [random.normalvariate(0, 1), 0.0, 0.0],
-            [0.0, random.normalvariate(0, 1), 0.0],
-            [0.0, 0.0, random.normalvariate(0, 1)],
+            [random.normalvariate(0, .01), 0.0, 0.0],
+            [0.0, random.normalvariate(0, .01), 0.0],
+            [0.0, 0.0, random.normalvariate(0, .01)],
         ])
         
         K_t = Sigma_t @ C_t.T @ np.linalg.inv(C_t @ Sigma_t @ C_t.T + Q_t)
